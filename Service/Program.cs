f732b38cb04e60fc65b3647e81f8f6986bdabf4e
Martynas Kan <martis347@gmail.com>
2014-12-18 17:25:30 +0200

    Start

---
+++ Service/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+using Warrior;
+using log4net;
+using log4net.Config;
+
+namespace Service
+{
+    public class Program
+    {
+        private static ILog _log;
+        static void Main(string[] args)
+        {
+            _log = LogManager.GetLogger("InfrastructureLogger");
+            XmlConfigurator.Configure(new FileInfo("..\\..\\App.config"));
+            var restartDelay = (int)TimeSpan.FromMinutes(1).TotalMinutes;
+
+            HostFactory.Run(config =>
+            {
+                config.Service<Business>(svc =>
+                {
+                    svc.ConstructUsing(s => new Business());
+                    svc.WhenStarted(s => s.Start());
+                    svc.WhenStopped(s => s.Stop());
+                    svc.WhenPaused(s => s.Pause());
+                    svc.WhenContinued(s => s.Continue());
+
+
+                });
+                _log.Info("Program is working");
+                config.EnablePauseAndContinue();
+                config.SetServiceName("MyService");
+                config.SetDisplayName("My service");
+                config.SetDescription("My service via Topshelf");
+                config.RunAsLocalService();
+                config.DependsOnEventLog();
+                config.StartAutomatically();
+                config.EnableServiceRecovery(recovery => recovery.RestartService(restartDelay));
+            });
+        }
+    }
+}
---
+++ Warrior/Business.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using log4net;
+using log4net.Config;
+
+namespace Warrior
+{
+    public class Business
+    {
+        private static ILog _log;
+        public Business()
+        {
+            _log = LogManager.GetLogger("BusinessLogger");
+            XmlConfigurator.Configure(new FileInfo("..\\..\\App.config"));
+            
+        }
+
+        public void Pause()
+        {
+            //for (int i = 0; i < 15; i++)
+            //{
+            //    Console.Beep();
+            //    Thread.Sleep(100);
+            //}
+            _log.Info("Program pause!");
+
+        }
+        public void Continue()
+        {
+            for (var i = 0; i < 15; i++)
+            {
+                Console.Beep();
+                Thread.Sleep(100);
+            }
+            _log.Info("Program continue!");
+
+        }
+        private bool write = true;
+        public void Start()
+        {
+            _log.Info("Program start!");
+
+            while (write)
+            {
+                Console.WriteLine("They see me runnin', they hatin'");
+                Console.Beep();
+                Thread.Sleep(1000);
+            }
+        }
+        public void Stop()
+        {
+            _log.Info("Program stop!");
+            write = false;
+        }
+
+        
+    }
+}
